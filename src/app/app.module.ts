aa26f0fb1e7f660976e3a8097c05cac51da5e48c
Jacobus <jahardin@uci.edu>
2017-06-25 19:40:58 -0700

    homepage work and framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { TeachersComponent } from './teachers/teachers.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { AnnouncementsComponent } from './announcements/announcements.component';
+import { FAQComponent } from './faq/faq.component';
+import { ContactComponent } from './contact/contact.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { AdministrationComponent } from './administration/administration.component';
+import { GalleryComponent } from './gallery/gallery.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'home/:id', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'teachers', component: TeachersComponent },
+  { path: 'faq', component: FAQComponent },
+  { path: 'testimonials', component: TestimonialsComponent },
+  { path: 'resources', component: ResourcesComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'administration', component: AdministrationComponent }  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    TeachersComponent,
+    TestimonialsComponent,
+    AnnouncementsComponent,
+    FAQComponent,
+    ContactComponent,
+    ResourcesComponent,
+    AdministrationComponent,
+    GalleryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
